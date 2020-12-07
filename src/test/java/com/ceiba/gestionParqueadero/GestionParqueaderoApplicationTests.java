f6fbf7452304fdf68ee1be648a3cbace575693d3
Juan Andrés López Osorio - Ceiba Software <juan.lopez@ceiba.local>
2020-12-06 21:42:19 -0500

    creación inicial del proyecto sonde se va a desarrollar el ADN Ceiba

---
+++ src/test/java/com/ceiba/gestionParqueadero/GestionParqueaderoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceiba.gestionParqueadero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionParqueaderoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
